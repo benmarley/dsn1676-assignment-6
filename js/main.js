766d3dd2d594af309013ab9835ab67477352c227
benmarley <benmarley@Bens-MacBook-Pro-3.local>
2015-03-11 19:19:13 -0400

    list functions but there is scroll wheel

--- js/main.js
+++ js/main.js
@@ -10,18 +10,25 @@ var $panel = $('.panel')
 var $bounce = $('.btn-bounce')
 var $circle = $('.circle')
 
+var $appendBtn = $('.btn-append')
+var $list = $('.list')
+
 $show.on('click', function () {
 	$box.toggleClass('opacity-animate');
-})
+});
 
 $btnmove.on('click', function () {
 	$diamond.toggleClass('diamond-move');
-})
+});
 
 $collapse.on('click', function () {
 	$panel.toggleClass('panel-collapse');
-})
+});
 
 $bounce.on('click', function () {
-	$circle.toggleClass('bouncing')
-})
+	$circle.toggleClass('bouncing');
+});
+
+$appendBtn.on('click', function () {
+	$list.append('<li>new list item</li>');
+});
