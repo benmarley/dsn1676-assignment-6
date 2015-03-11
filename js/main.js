5417edc7badd796abe4fc79e56115e42f0e3c506
benmarley <benmarley@Bens-MacBook-Pro-3.local>
2015-03-11 14:38:50 -0400

    added panel collapse

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ var $box = $('.box');
 var $btnmove = $('.btn-move')
 var $diamond = $('.diamond')
 
+var $collapse = $('.btn-collapse-expand')
+var $panel = $('.panel')
+
 $show.on('click', function () {
 	$box.toggleClass('opacity-animate');
 })
 
 $btnmove.on('click', function () {
 	$diamond.toggleClass('diamond-move');
+})
+
+$collapse.on('click', function () {
+	$panel.toggleClass('panel-collapse');
 })
